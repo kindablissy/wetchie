ee630e424aefe680011564511d16e62e2b3b1bfc
nishedh <restricted.karki@gmail.com>
2023-06-28 10:49:46 +0545

    drawing experiments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ROOT = document.getElementById("root");
 
-const Element = (style) => {
-  const element = document.createElement("div");
+const Element = (style, tag = "div") => {
+  const element = document.createElement(tag);
   const styles = Object.keys(style);
   for (let i in styles) {
     element.style[styles[i]] = style[styles[i]];
@@ -15,6 +15,69 @@ const ColorBox = (style) => {
   return element;
 };
 
-ROOT.appendChild(
-  ColorBox({ height: "30px", width: "30px", backgroundColor: "red" })
-);
+const DrawingBox = (style) => {
+  const element = Element(style, "canvas");
+
+  return element;
+};
+
+const drawingbox = document.getElementById("canvas");
+drawingbox.style.backgroundColor = "powderblue";
+ROOT.appendChild(drawingbox);
+
+var recorded_mousePos = {};
+recorded_mousePos = { old: { x: 0, y: 0 } };
+var drawing = false;
+
+const createContext = (element, position) => {
+  const ctx = element.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(position.x, position.y);
+  return ctx;
+};
+
+var currentContext = null;
+
+const sketch = (ctx, e) => {
+  if (!drawing) return;
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (
+    true ||
+    (recorded_mousePos.old.x - x) * (recorded_mousePos.old.x - x) +
+      (recorded_mousePos.old.y - y) * (recorded_mousePos.old.y - y) <
+      100
+  ) {
+    console.log(x);
+    ctx.moveTo(recorded_mousePos.old.x, recorded_mousePos.old.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.closePath();
+    recorded_mousePos.old = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+  }
+};
+
+drawingbox.onmousedown = (e) => {
+  if (drawing) return;
+  else drawing = !drawing;
+  currentContext = createContext(drawingbox, {
+    x: MouseEvent.offsetX,
+    y: MouseEvent.offsetY,
+  });
+  recorded_mousePos.old = {
+    x: MouseEvent.offsetX,
+    y: MouseEvent.offsetY,
+  };
+  console.log("clicked");
+};
+
+drawingbox.onmouseup = () => {
+  if (!drawing) return;
+  drawing = false;
+  recorded_mousePos = { old: { x: 0, y: 0 } };
+};
+
+document.onmousemove = (e) => sketch(currentContext, e);
